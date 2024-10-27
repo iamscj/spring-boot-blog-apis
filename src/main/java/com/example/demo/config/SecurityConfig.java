d453a90369a2a1d6afa36864c0b38a043ad19ff9
shreyasa.joshi <shreyasa.joshi.official@gmail.com>
2024-10-27 23:48:05 +0530

    adds security & use database auth

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,15 +1,23 @@
 package com.example.demo.config;
 
+import com.example.demo.security.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -21,5 +29,15 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());  // Use basic authentication
 
         return http.build();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/demo/repositories/UserRepo.java
+++ src/main/java/com/example/demo/repositories/UserRepo.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Integer> {
 
 //    @Query("SELECT u FROM User u WHERE u.email = :email")
@@ -13,4 +15,7 @@ public interface UserRepo extends JpaRepository<User, Integer> {
     User findByEmail(String email);
 
     User findByName(String name);
+
+//    @Query("SELECT u FROM User u WHERE u.email = :email")
+//    Optional<User> findUserByEmail(@Param("email") String email);
 }
